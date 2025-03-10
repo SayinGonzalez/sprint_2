815dc06fc8be7204a09ea2a4b3aa92f70e14f22f
SayinGonzalez <sayingonzalez@gmail.com>
2025-03-09 22:39:01 -0300

    Agregado conexión y esquema de la base de datos

---
+++ app.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb+srv://Grupo-06:grupo06@cursadanodejs.ls9ii.mongodb.net/Node-js')
+.then(() => console.log('Conexión exitosa a MongoDB'))
+.catch(error => console.error('Error al conectar a MongoDB:', error));
+
+const superheroSchema = new mongoose.Schema({
+    nombreSuperHeroe: { type: String, required: true },
+    nombreReal: { type: String, required: true },
+    edad: { type: Number, min: 0 },
+    planetaOrigen: { type: String, default: 'Desconocido' },
+    debilidad: String,
+    poderes: [String],
+    aliados: [String],
+    enemigos: [String],
+    createdAt: { type: Date, default: Date.now },
+    creador: String
+}, { collection: 'Grupo-06' });
+
+const SuperHero = mongoose.model('SuperHero', superheroSchema);
